e5a2b4ff5f30e1bff5e585c22020df2bdff7b199
frinhanifrinhani <frinhani.frinhani@gmail.com>
2017-05-16 15:40:59 -0300

    Layout: Tipo de perfil

--- module/Application/src/Application/Controller/ActionHelper/ACL.php
+++ module/Application/src/Application/Controller/ActionHelper/ACL.php
@@ -9,18 +9,10 @@ use Zend\Permissions\Acl\Resource\GenericResource as Resource;
 class ACL extends BaseAcl {
 
     public function __construct() {
-//        $this->addRole(new Role('3'));
-//        $this->addRole(new Role('2'), '3');
-//        $this->addRole(new Role('1'), '2');
 
-        $this->addRole(new Role('scrumteam'));
-        $this->addRole(new Role('scrummaster'));
-        $this->addRole(new Role('productowner'));
-
-//        
-//        $this->addResource(new Resource('IndexController'));
-//        $this->addResource(new Resource('ParticipanteController'));
-//        $this->addResource(new Resource('ProjetoController'));
+        $this->addRole(new Role('Scrum Team'));
+        $this->addRole(new Role('Scrum Master'));
+        $this->addRole(new Role('Product Owner'));
 
         $this->addResource(new Resource('Application\Controller\Index'));
         $this->addResource(new Resource('Application\Controller\Participante'));
@@ -32,30 +24,30 @@ class ACL extends BaseAcl {
 
 
         //Scrum Team - 3
-        $this->allow('scrumteam', 'Application\Controller\Index', 'inicio');
-        $this->allow('scrumteam', 'Application\Controller\Participante', array('alterarsenha'));
-        $this->allow('scrumteam', 'Application\Controller\Projeto', array('listar', 'gerenciar'));
-        $this->allow('scrumteam', 'Application\Controller\Sprint', array('listar'));
-        $this->allow('scrumteam', 'Application\Controller\ProductBacklog', array('listar'));
-        $this->allow('scrumteam', 'Application\Controller\SprintBacklog', array('escolher', 'listar', 'cadastrar', 'editar')); //verificar excluir
+        $this->allow('Scrum Team', 'Application\Controller\Index', 'inicio');
+        $this->allow('Scrum Team', 'Application\Controller\Participante', array('alterarsenha'));
+        $this->allow('Scrum Team', 'Application\Controller\Projeto', array('listar', 'gerenciar'));
+        $this->allow('Scrum Team', 'Application\Controller\Sprint', array('listar'));
+        $this->allow('Scrum Team', 'Application\Controller\ProductBacklog', array('listar'));
+        $this->allow('Scrum Team', 'Application\Controller\SprintBacklog', array('escolher', 'listar', 'cadastrar', 'editar')); //verificar excluir
 
         //Scrum Master - 2
-        $this->allow('scrummaster', 'Application\Controller\Index', 'inicio');
-        $this->allow('scrummaster', 'Application\Controller\Participante', array('listar', 'cadastrar', 'editar', 'excluir', 'alterarsenha'));
-        $this->allow('scrummaster', 'Application\Controller\Projeto', array('listar', 'gerenciar', 'relatorio'));
-        $this->allow('scrummaster', 'Application\Controller\Sprint', array('listar', 'cadastrar', 'editar', 'excluir')); //verificar editar e excluir
-        $this->allow('scrummaster', 'Application\Controller\ProductBacklog', array('listar'));
-        $this->allow('scrummaster', 'Application\Controller\SprintBacklog', array('escolher', 'listar', 'cadastrar', 'editar', 'excluir'));
-        $this->allow('scrummaster', 'Application\Controller\ProductBacklogPorSprint', array('escolher', 'listar'));
+        $this->allow('Scrum Master', 'Application\Controller\Index', 'inicio');
+        $this->allow('Scrum Master', 'Application\Controller\Participante', array('listar', 'cadastrar', 'editar', 'excluir', 'alterarsenha'));
+        $this->allow('Scrum Master', 'Application\Controller\Projeto', array('listar', 'gerenciar', 'relatorio'));
+        $this->allow('Scrum Master', 'Application\Controller\Sprint', array('listar', 'cadastrar', 'editar', 'excluir')); //verificar editar e excluir
+        $this->allow('Scrum Master', 'Application\Controller\ProductBacklog', array('listar'));
+        $this->allow('Scrum Master', 'Application\Controller\SprintBacklog', array('escolher', 'listar', 'cadastrar', 'editar', 'excluir'));
+        $this->allow('Scrum Master', 'Application\Controller\ProductBacklogPorSprint', array('escolher', 'listar'));
 
         //Product Owner - 1
-        $this->allow('productowner', 'Application\Controller\Index', 'inicio');
-        $this->allow('productowner', 'Application\Controller\Participante', array('listar', 'cadastrar', 'editar', 'excluir', 'alterarsenha'));
-        $this->allow('productowner', 'Application\Controller\Projeto', array('listar', 'cadastrar', 'editar', 'excluir', 'gerenciar', 'relatorio'));
-        $this->allow('productowner', 'Application\Controller\Sprint', array('listar', 'cadastrar', 'editar', 'excluir'));
-        $this->allow('productowner', 'Application\Controller\ProductBacklog', array('listar', 'cadastrar', 'editar', 'excluir'));
-        $this->allow('productowner', 'Application\Controller\SprintBacklog', array('escolher', 'listar', 'cadastrar', 'editar', 'excluir'));
-        $this->allow('productowner', 'Application\Controller\ProductBacklogPorSprint', array('escolher', 'listar'));
+        $this->allow('Product Owner', 'Application\Controller\Index', 'inicio');
+        $this->allow('Product Owner', 'Application\Controller\Participante', array('listar', 'cadastrar', 'editar', 'excluir', 'alterarsenha'));
+        $this->allow('Product Owner', 'Application\Controller\Projeto', array('listar', 'cadastrar', 'editar', 'excluir', 'gerenciar', 'relatorio'));
+        $this->allow('Product Owner', 'Application\Controller\Sprint', array('listar', 'cadastrar', 'editar', 'excluir'));
+        $this->allow('Product Owner', 'Application\Controller\ProductBacklog', array('listar', 'cadastrar', 'editar', 'excluir'));
+        $this->allow('Product Owner', 'Application\Controller\SprintBacklog', array('escolher', 'listar', 'cadastrar', 'editar', 'excluir'));
+        $this->allow('Product Owner', 'Application\Controller\ProductBacklogPorSprint', array('escolher', 'listar'));
     }
 
 }
--- module/Application/src/Application/Controller/ActionHelper/MESACL.php
+++ module/Application/src/Application/Controller/ActionHelper/MESACL.php
@@ -16,7 +16,6 @@ use Zend\Authentication\Validator\Authentication;
 use Zend\Authentication\AuthenticationService;
 //use Zend\Mvc\Controller\Plugin\Redirect;
 use Zend\Session\Container;
-
 use Application\Controller\ActionHelper\ACL;
 
 class MESACL extends AbstractPlugin {
@@ -29,13 +28,26 @@ class MESACL extends AbstractPlugin {
 
         $this->autenticado = new AuthenticationService();
         $this->dados = $this->autenticado->getIdentity();
- 
+
     }
-    
-    
-    public function container(){
-        
+
+    public function container() {
+
         $container = new Container();
+
+        switch ($container->cod_tipo_participante){
+            case 1:
+                $this->dados->tipo_participante = 'Product Owner';
+                break;
+            
+            case 2:
+                $this->dados->tipo_participante = 'Scrum Master';
+                break;
+            
+            case 3:
+                $this->dados->tipo_participante = 'Scrum Team';
+                break;
+        }
         
         $container->cod_participante = $this->dados->cod_participante;
         $container->cod_tipo_participante = $this->dados->cod_tipo_participante;
@@ -43,8 +55,8 @@ class MESACL extends AbstractPlugin {
         $container->nome_participante = $this->dados->nome_participante;
         $container->email_participante = $this->dados->email_participante;
         $container->senha_participante = $this->dados->senha_participante;
-        
-       return array(
+
+        return array(
             'cod_participante' => $container->cod_participante,
             'cod_tipo_participante' => $container->cod_tipo_participante,
             'tipo_participante' => $container->tipo_participante,
@@ -53,36 +65,22 @@ class MESACL extends AbstractPlugin {
             'senha_participante' => $container->senha_participante,
         );
     }
-    public function acl(){
+
+    public function acl() {
         $controller = $this->getController();
 
         $controllerName = $controller->params('controller');
         $actionName = $controller->params('action');
-        
-        switch ($this->container()['cod_tipo_participante']){
-            case 1:
-                $perfil = 'productowner';
-                break;
-            
-            case 2:
-                $perfil = 'scrummaster';
-                break;
-            
-            case 3:
-                $perfil = 'scrumteam';
-                break;
-        }   
-//        echo "Rota: $controllerName - $actionName <br />";
-//        echo "Perfil: $perfil<br />";
-//        echo 'aqu';
+
         $acl = new ACL();
-        if ($acl->isAllowed($perfil, $controllerName, $actionName)) {
+        if ($acl->isAllowed($this->dados->tipo_participante, $controllerName, $actionName)) {
             return true;
         } else {
-            
+
             return false;
         }
     }
+
     public function verificarAutenticacao() {
         if (!$this->autenticado->hasIdentity()) {
             return false;
@@ -94,22 +92,21 @@ class MESACL extends AbstractPlugin {
     public function permitir() {
         $autenticado = $this->verificarAutenticacao();
         $permitido = $this->acl();
-        
+
         $container = new Container();
+
 //        $this->layout()->nomeParticipante='Thiago';
         if ($autenticado == false) {
             $controller = $this->getController();
             $redirector = $controller->getPluginManager()->get('Redirect');
             return $redirector->toRoute('login');
         }
-        
-        if($permitido == false){
+
+        if ($permitido == false) {
             $controller = $this->getController();
             $redirector = $controller->getPluginManager()->get('Redirect');
             return $redirector->toRoute('acesso-negado');
-              
         }
-
     }
 
 }
